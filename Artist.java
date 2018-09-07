2b29cf1abcc42f7c679919704e6074b612e4dee4
AKSHAY TENKALE <tenkaleakshay94@gmail.com>
2018-09-06 23:26:38 -0400

    Add files via upload

---
+++ Artist.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Artist {
+	
+	private String artistName;
+	private int artistId;
+	private String artistInfo;
+	private String location;
+	private String albumLyric;
+	private String imageLink;
+	public String getArtistName() {
+		return artistName;
+	}
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+	public int getArtistId() {
+		return artistId;
+	}
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
+	public String getArtistInfo() {
+		return artistInfo;
+	}
+	public void setArtistInfo(String artistInfo) {
+		this.artistInfo = artistInfo;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getAlbumLyric() {
+		return albumLyric;
+	}
+	public void setAlbumLyric(String albumLyric) {
+		this.albumLyric = albumLyric;
+	}
+	public String getImageLink() {
+		return imageLink;
+	}
+	public void setImageLink(String imageLink) {
+		this.imageLink = imageLink;
+	}
+
+
+}
